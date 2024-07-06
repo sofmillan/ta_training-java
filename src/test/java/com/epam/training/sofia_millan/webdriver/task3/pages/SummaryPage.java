956831c0d6c8ba1cfd3ca9cd803176d40df3ed2c
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-07-06 09:40:33 -0500

    Adding page factory

--- src/test/java/com/epam/training/sofia_millan/webdriver/task3/pages/SummaryPage.java
+++ src/test/java/com/epam/training/sofia_millan/webdriver/task3/pages/SummaryPage.java
@@ -4,6 +4,9 @@ import com.epam.training.sofia_millan.webdriver.task3.utils.BrowserUtils;
 import com.epam.training.sofia_millan.webdriver.task3.utils.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,18 +15,19 @@ import java.time.Duration;
 public class SummaryPage {
     private WebDriver driver;
     private WebDriverWait wait;
-    private By estimatedCostTitle = By.xpath("//h4[contains(@class, 'n8xu5')]");
-
+    @FindBy(xpath = "//h4[contains(@class, 'n8xu5')]")
+    private WebElement estimatedCostTitle;
 
     public SummaryPage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
     public double getEstimate(){
         BrowserUtils.changeTab(driver,1);
         String estimate = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(estimatedCostTitle)).getText();
+                .visibilityOf(estimatedCostTitle)).getText();
         return Utils.convertStringToDouble(estimate);
     }
 
