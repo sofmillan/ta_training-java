9df3c1650eb86e662515b2100dd83b2350f5407c
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-07-06 09:37:35 -0500

    Adding page factory

--- src/test/java/com/epam/training/sofia_millan/webdriver/task3/pages/CalculatorPage.java
+++ src/test/java/com/epam/training/sofia_millan/webdriver/task3/pages/CalculatorPage.java
@@ -4,6 +4,8 @@ import com.epam.training.sofia_millan.webdriver.task3.utils.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -13,14 +15,22 @@ public class CalculatorPage {
     private final static String URL = "https://cloud.google.com/products/calculator?hl=es";
     private WebDriver driver;
     private WebDriverWait wait;
-    private By numberInstancesInput = By.xpath("//span[@class='qdOxv-fmcmS-wGMbrd-sM5MNb']//input[@type='number']");
-    private By product = By.xpath("//h2[text()='Compute Engine']");
-    private By addGPUButton = By.xpath("//button[@aria-label='Add GPUs']");
-    private By shareButton = By.xpath("//span[contains(@class, 'FOBRw-RLmnJb')]");
-    private By openEstimateButton = By.xpath("//a[contains(@class, 'rP2xkc')]");
-    private By estimatedCost = By.xpath("//span[contains(@class, 'MyvX5d')]");
-    private By addToEstimateButton = By.cssSelector(".jirROd button");
-    private By updateMessage = By.xpath("//div[contains(@class,'Z7Qi9d') and contains(@class, ' HY0Uh')]");
+    @FindBy(xpath ="//span[@class='qdOxv-fmcmS-wGMbrd-sM5MNb']//input[@type='number']")
+    private WebElement numberInstancesInput;
+    @FindBy(xpath ="//h2[text()='Compute Engine']")
+    private WebElement product;
+    @FindBy(xpath ="//button[@aria-label='Add GPUs']")
+    private WebElement addGPUButton;
+    @FindBy(xpath ="//span[contains(@class, 'FOBRw-RLmnJb')]")
+    private WebElement shareButton;
+    @FindBy(xpath ="//a[contains(@class, 'rP2xkc')]")
+    private WebElement openEstimateButton;
+    @FindBy(xpath ="//span[contains(@class, 'MyvX5d')]")
+    private WebElement estimatedCost;
+    @FindBy(css =".jirROd button")
+    private WebElement addToEstimateButton;
+    @FindBy(xpath = "//div[contains(@class,'Z7Qi9d') and contains(@class, ' HY0Uh')]")
+    private WebElement updateMessage;
     private String baseDropDown = "//div[contains(@class, 'VfPpkd-TkwUic') and .//span[contains(@class, 'VfPpkd-NLUYnc-V67aGc-OWXEXe-TATcMc-KLRBe') and contains(text(), '%s')]]";
     private String baseListItem = "//li[@data-value='%s']";
     private String baseLabel = "//label[contains(@class, 'zT2df') and @for='%s']";
@@ -28,7 +38,8 @@ public class CalculatorPage {
 
     public CalculatorPage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
     public void openPage(){
@@ -39,8 +50,8 @@ public class CalculatorPage {
     public void fillForm(){
         addProductToEstimate(product);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(numberInstancesInput)).clear();
-        driver.findElement(numberInstancesInput).sendKeys("4");
+        wait.until(ExpectedConditions.visibilityOf(numberInstancesInput)).clear();
+        numberInstancesInput.sendKeys("4");
 
         findDropDown("Operating System").click();
         findItemInList("free-debian-centos-coreos-ubuntu-or-byol-bring-your-own-license").click();
@@ -56,7 +67,7 @@ public class CalculatorPage {
         findDropDown("Machine type").click();
         findItemInList("n1-standard-8").click();
 
-        driver.findElement(addGPUButton).click();
+        addGPUButton.click();
 
         findDropDown("GPU Model").click();
         findItemInList("nvidia-tesla-v100").click();
@@ -74,32 +85,33 @@ public class CalculatorPage {
     }
 
     public void shareEstimate(){
-        driver.findElement(shareButton).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(openEstimateButton)).click();
+        wait.until(ExpectedConditions.invisibilityOf(updateMessage));
+        shareButton.click();
+        wait.until(ExpectedConditions.visibilityOf(openEstimateButton)).click();
     }
 
     public Double getEstimate(){
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(updateMessage));
-        String estimate = driver.findElement(estimatedCost).getText();
+        wait.until(ExpectedConditions.invisibilityOf(updateMessage));
+        String estimate = estimatedCost.getText();
         return Utils.convertStringToDouble(estimate);
     }
 
     private WebElement findDropDown(String title){
-        String locator = String.format(baseDropDown, title);
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        String dropDownLocator = String.format(baseDropDown, title);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(dropDownLocator)));
     }
     private WebElement findLabel(String title){
-        String locator = String.format(baseLabel, title);
-        return driver.findElement(By.xpath(locator));
+        String labelLocator = String.format(baseLabel, title);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(labelLocator)));
     }
 
     private WebElement findItemInList(String value){
-        String locator = String.format(baseListItem, value);
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        String listItemLocator = String.format(baseListItem, value);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(listItemLocator)));
     }
 
-    private void addProductToEstimate(By productLocator){
-        driver.findElement(addToEstimateButton).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productLocator)).click();
+    private void addProductToEstimate(WebElement product){
+        addToEstimateButton.click();
+        wait.until(ExpectedConditions.visibilityOf(product)).click();
     }
 }
--- src/test/java/com/epam/training/sofia_millan/webdriver/task3/utils/Utils.java
+++ src/test/java/com/epam/training/sofia_millan/webdriver/task3/utils/Utils.java
@@ -2,12 +2,11 @@ package com.epam.training.sofia_millan.webdriver.task3.utils;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.Locale;
 
 public class Utils {
     public static double convertStringToDouble(String amount)  {
         amount = amount.replaceAll("[^\\d,\\.]", "").replace(" ", "");
-        NumberFormat format = NumberFormat.getInstance(Locale.GERMAN);
+        NumberFormat format = NumberFormat.getInstance();
         Number number;
         try {
             number = format.parse(amount);
